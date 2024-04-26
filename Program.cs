5550d5e3e7dbf0fd1b834e47a164ed7327a47f4c
Amaral1973 <61523977+Amaral1973@users.noreply.github.com>
2024-04-25 22:17:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+int num, resultado;
+
+WriteLine(">>>> TABUADA <<<<");
+WriteLine("Digite um número inteiro:");
+num  = Convert.ToInt32(ReadLine());
+
+for (int i = 1; i <= 10; i++)
+{
+    resultado = num * i;
+    WriteLine(num + " X " + i + " = " + resultado.ToString());
+}
